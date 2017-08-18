334b76431a18531b2c8a24cdc58c27300c2878d2
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-18 09:19:53 -0400

    Getting variable input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Telematics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the Vechile Number: ");
+            var vinInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Mileage: ");
+            var odometerInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Consumption: ");
+            var consumptionInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Miles at Last Oil Change: ");
+            var lastOilChange = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Engine Size: ");
+            var engineSize = double.Parse(Console.ReadLine());
+            
         }
     }
 }
---
+++ TelematicsService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Telematics
+{
+    public class TelematicsService
+    {
+        public void Report(VehicleInfo vehicleInfo)
+        {
+            
+        }
+    }
+}

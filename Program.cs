80fb05647a389291df926129b44bdd66edef7357
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-21 17:19:52 -0400

    got from jake

--- Program.cs
+++ Program.cs
@@ -27,8 +27,7 @@ namespace Telematics
 
             var newVehicle = new VehicleInfo(vinInput, odometerInput, consumptionInput, lastOilChange, engineSize); 
             new TelematicsService().Report(newVehicle);
-            new TelematicsService().deJson(newVehicle);
-            new TelematicsService().updateHTML(newVehicle);
+            new TelematicsService().GenerateHTMLReport(newVehicle);
         }
     }
 }
--- TelematicsService.cs
+++ TelematicsService.cs
@@ -1,8 +1,7 @@
 using System;
-using System.IO;
-using System.Collections;
-using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Telematics
 {
@@ -14,29 +13,59 @@ namespace Telematics
             using (var writer = new StreamWriter(File.Open($"{vehicleInfo.VIN}.json", FileMode.OpenOrCreate)))
             using (var jsonWriter = new JsonTextWriter(writer))
             {
+
                 serializer.Serialize(writer, vehicleInfo);
             }
         }
-
-        public void deJson(VehicleInfo vehicleInfo)
+        public void GenerateHTMLReport(VehicleInfo vehicleInfo)
         {
-            string[] files = System.IO.Directory.GetFiles("/Users/brandyn/dotnet/Telematics", "*.json");
-            ///LIST CREATION GOES HERE
+            string[] files = System.IO.Directory.GetFiles("/Users/brandyn/dotnet/telematics", "*.json");
+            ///add stuff to a list here
             List<object> vehicleList = new List<object>();
+            var totalOdometer = 0d;
+            var itemTemplate = $@"<table align='center' border='1'>
+          <tr>
+              <th>VIN</th><th>Odometer (miles)</th><th>Consumption (gallons)</th><th>Last Oil Change</th><th>Engine Size (liters)</th>
+          </tr>
+          <tr>
+              <td align='center'>{0}</td><td align='center'>{1}</td><td align='center'>{2}</td><td align='center'>{3}</td><td align='center'>{4}</td>
+          </tr>
+      </table>";
+      var tableHtml = string.Empty;
+
             foreach (var item in files)
             {
                 using (StreamReader file = File.OpenText(item))
                 {
-                    var vehicleInfoObj = JsonConvert.DeserializeObject<VehicleInfo>(file.ReadToEnd());
-                    vehicleList.Add(vehicleInfoObj);
+                    var vehicleInfo2 = JsonConvert.DeserializeObject<VehicleInfo>(file.ReadToEnd());
+                    vehicleList.Add(vehicleInfo2);
+                    totalOdometer += vehicleInfo2.Odometer;
+                    tableHtml += string.Format($"{itemTemplate}",vehicleInfo2.VIN,vehicleInfo2.Odometer,vehicleInfo2.Consumption,vehicleInfo2.OdometerLastOilChange,vehicleInfo2.EngineSize);
                 }
             }
-        }
+            var odometerAverage = totalOdometer/vehicleList.Count;
+            string html = $@"<html>
+    <title>Vehicle Telematics Dashboard</title>
+    <body>
+      <h1 align='center'>Averages for # vehicles</h1>
+      <table align='center'>
+          <tr>
+              <th>Odometer (miles) |</th><th>Consumption (gallons) |</th><th>Last Oil Change |</th><th>Engine Size (liters)</th>
+          </tr>
+          <tr>
+              <td align='center'>{odometerAverage}</td><td align='center'>2</td><td align='center'>3</td align='center'><td align='center'>4</td>
+          </tr>
+      </table>
+      <h1 align='center'>History</h1>
+      {tableHtml}
+    </body>
+  </html>";
+  using (var writer = new StreamWriter(File.Open($"Dashboard.html", FileMode.OpenOrCreate)))
+            {
 
-        public void updateHTML(VehicleInfo vehicleinf)
-        {
-            string html = File.ReadAllText("/Users/brandyn/dotnet/Telematics/Dashboard.html");
-            Console.WriteLine(html);
+                writer.WriteLine(html);
+            }
         }
     }
+
 }
